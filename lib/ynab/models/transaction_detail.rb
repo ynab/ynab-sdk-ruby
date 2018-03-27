efa32ef3e7fcb366761c9fc7e75e44d6dc2c7eda
Brady Holt <brady.holt@gmail.com>
2018-03-27 13:16:03 -0500

    Generate latest from server spec (version at 0.6.0)

--- lib/ynab/models/transaction_detail.rb
+++ lib/ynab/models/transaction_detail.rb
@@ -44,6 +44,8 @@ module YnabApi
     # If the Transaction was imported, this field is a unique (by account) import identifier.  If this transaction was imported through File Based Import or Direct Import and not through the API, the import_id will have the format: 'YNAB:[milliunit_amount]:[iso_date]:[occurrence]'.  For example, a transaction dated 2015-12-30 in the amount of -$294.23 USD would have an import_id of 'YNAB:-294230:2015-12-30:1'.  If a second transaction on the same account was imported and had the same date and same amount, its import_id would be 'YNAB:-294230:2015-12-30:2'.
     attr_accessor :import_id
 
+    attr_accessor :account_name
+
     attr_accessor :payee_name
 
     attr_accessor :category_name
@@ -88,6 +90,7 @@ module YnabApi
         :'category_id' => :'category_id',
         :'transfer_account_id' => :'transfer_account_id',
         :'import_id' => :'import_id',
+        :'account_name' => :'account_name',
         :'payee_name' => :'payee_name',
         :'category_name' => :'category_name',
         :'subtransactions' => :'subtransactions'
@@ -109,6 +112,7 @@ module YnabApi
         :'category_id' => :'String',
         :'transfer_account_id' => :'String',
         :'import_id' => :'String',
+        :'account_name' => :'String',
         :'payee_name' => :'String',
         :'category_name' => :'String',
         :'subtransactions' => :'Array<SubTransaction>'
@@ -169,6 +173,10 @@ module YnabApi
 
       if attributes.has_key?(:'import_id')
         self.import_id = attributes[:'import_id']
+      end
+
+      if attributes.has_key?(:'account_name')
+        self.account_name = attributes[:'account_name']
       end
 
       if attributes.has_key?(:'payee_name')
@@ -239,6 +247,10 @@ module YnabApi
         invalid_properties.push("invalid value for 'import_id', import_id cannot be nil.")
       end
 
+      if @account_name.nil?
+        invalid_properties.push("invalid value for 'account_name', account_name cannot be nil.")
+      end
+
       if @payee_name.nil?
         invalid_properties.push("invalid value for 'payee_name', payee_name cannot be nil.")
       end
@@ -273,6 +285,7 @@ module YnabApi
       return false if @category_id.nil?
       return false if @transfer_account_id.nil?
       return false if @import_id.nil?
+      return false if @account_name.nil?
       return false if @payee_name.nil?
       return false if @category_name.nil?
       return false if @subtransactions.nil?
@@ -316,6 +329,7 @@ module YnabApi
           category_id == o.category_id &&
           transfer_account_id == o.transfer_account_id &&
           import_id == o.import_id &&
+          account_name == o.account_name &&
           payee_name == o.payee_name &&
           category_name == o.category_name &&
           subtransactions == o.subtransactions
@@ -330,7 +344,7 @@ module YnabApi
     # Calculates hash code according to all attributes.
     # @return [Fixnum] Hash code
     def hash
-      [id, date, amount, memo, cleared, approved, flag_color, account_id, payee_id, category_id, transfer_account_id, import_id, payee_name, category_name, subtransactions].hash
+      [id, date, amount, memo, cleared, approved, flag_color, account_id, payee_id, category_id, transfer_account_id, import_id, account_name, payee_name, category_name, subtransactions].hash
     end
 
     # Builds the object from hash
