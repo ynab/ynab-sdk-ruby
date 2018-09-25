8edb7052f287d1a4ff6c337620809823c0ded016
Brady Holt <brady.holt@gmail.com>
2018-09-25 17:42:44 -0500

    Add spec for updating month category

--- spec/api/categories_spec.rb
+++ spec/api/categories_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'categories' do
   let(:access_token) { '9f1a2c4842b614a771aaae9220fc54ae835e298c4654dc2c9205fc1d7bd1a045' }
   let(:budget_id) { 'f419ac25-6217-4175-88dc-c3136ff5f6fd' }
+  let(:category_id) { 'acd2a771-0a56-4dba-97bd-3952b0879282' }
   let(:client) { YNAB::API.new(access_token, 'api.localhost:3000', false) }
   let (:instance) { client.categories }
 
@@ -45,12 +46,23 @@ describe 'categories' do
     end
   end
 
-  describe 'GET /budgets/{budget_id}/category/{category_id}' do
+  describe 'GET /budgets/{budget_id}/categories/{category_id}' do
     it "returns a category" do
       VCR.use_cassette("category") do
-        response = instance.get_category_by_id(budget_id, '574fac19-537d-46a8-be2b-d2f875691165')
+        response = instance.get_category_by_id(budget_id, category_id)
         expect(response.data.category).to be
         expect(response.data.category.name).to eq 'Groceries'
+      end
+    end
+  end
+
+  describe 'PATCH /budgets/{budget_id}/months/{month}/categories/{category_id}' do
+    it "returns a category" do
+      VCR.use_cassette("patch_month_category") do
+        response = instance.update_month_category(budget_id, '2018-02-01', category_id, month_category: { budgeted: 20382 })
+        expect(response.data.category).to be
+        expect(response.data.category.name).to eq 'Groceries'
+        expect(response.data.category.budgeted).to eq 20382
       end
     end
   end
