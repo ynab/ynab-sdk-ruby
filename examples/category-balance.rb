1cf4261ee20c95171d33cb394206704f07db2a15
Brady Holt <brady.holt@gmail.com>
2019-01-21 17:59:50 -0600

    Some minor formatting and styling updates to examples

--- examples/category-balance.rb
+++ examples/category-balance.rb
@@ -1,26 +1,23 @@
 require 'ynab'
 
-def print_category_info
-  access_token = ENV['YNAB_ACCESS_TOKEN']
-  ynab = YNAB::API.new(access_token)
+access_token = ENV['YNAB_ACCESS_TOKEN']
+ynab = YNAB::API.new(access_token)
 
-  budget_id = ENV['YNAB_BUDGET_ID']
-  category_id = 'a191ac84-de09-not-real-6c5ed8cfdabe'
+budget_id = ENV['YNAB_BUDGET_ID']
+category_id = 'a191ac84-de09-not-real-6c5ed8cfdabe'
 
-  begin
-    puts 'Fetching category...'
-    category_response = ynab.categories.get_category_by_id(budget_id, category_id)
-    category = category_response.data.category
+begin
+  puts 'Fetching category...'
 
-    puts "==========="
-    puts "CATEGORY"
-    puts "==========="
-    puts "      Name: #{category.name}"
-    puts "  Budgeted: #{category.budgeted}"
-    puts "   Balance: #{category.balance}"
-  rescue YNAB::ApiError => e
-    puts "ERROR: id=#{e.id}; name=#{e.name}; detail: #{e.detail}"
-  end
+  category_response = ynab.categories.get_category_by_id(budget_id, category_id)
+  category = category_response.data.category
+
+  puts "==========="
+  puts "CATEGORY"
+  puts "==========="
+  puts "      Name: #{category.name}"
+  puts "  Budgeted: #{category.budgeted}"
+  puts "   Balance: #{category.balance}"
+rescue YNAB::ApiError => e
+  puts "ERROR: id=#{e.id}; name=#{e.name}; detail: #{e.detail}"
 end
-
-print_category_info
